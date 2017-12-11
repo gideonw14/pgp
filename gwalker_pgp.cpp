32c4cb5fa49b70f3c09dd2ff6dddb91ac8dacd86
Gideon Walker <gideonw14@gmail.com>
2017-12-11 13:18:24 -0600

    add semis

--- gwalker_pgp.cpp
+++ gwalker_pgp.cpp
@@ -19,30 +19,30 @@ int main (){
 
   cout << "Enter the name of the file that contains Alice’s public-private key pair:" << endl;
   cin >> alice_keys_fn;
-  inputs.push_back(alice_keys_fn)
+  inputs.push_back(alice_keys_fn);
   cout << "Enter the name of the file that contains Bob’s public-private key pair:" << endl;
   cin >> bob_keys_fn;
-  inputs.push_back(bob_keys_fn)
+  inputs.push_back(bob_keys_fn);
   cout << "Enter the name of the file that contains Alice’s plaintext message:" << endl;
   cin >> alice_text_fn;
-  inputs.push_back(alice_text_fn)
+  inputs.push_back(alice_text_fn);
   cout << " Enter the output file name to store Alice’s authenticated message:" << endl;
   cin >> alice_auth_fn;
-  inputs.push_back(alice_auth_fn)
+  inputs.push_back(alice_auth_fn);
   cout << "Enter the output file name to store the verification steps performed by Bob:" << endl;
   cin >> bob_verify_fn;
-  inputs.push_back(bob_verify_fn)
+  inputs.push_back(bob_verify_fn);
   cout << "Enter the output file name to store Alice’s encrypted message:" << endl;
   cin >> alice_encrypt_fn;
-  inputs.push_back(alice_encrypt_fn)
+  inputs.push_back(alice_encrypt_fn);
   cout << "Enter the output file name to store Bob’s decryption of Alice’s plaintext message:" << endl;
   cin >> bob_decrypt_fn;
-  inputs.push_back(bob_decrypt_fn)
+  inputs.push_back(bob_decrypt_fn);
 
   for(int i=0; i<inputs.size(); i++){
     cout << inputs[i] << " ";
   }
   cout << endl;
-  
+
   return 0;
 }
