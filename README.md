a327f4fc225e3294e8012ce65a5b531c2b33ab33
gideonw14 <gideonw14@gmail.com>
2017-12-14 13:36:21 -0600

    It is finished

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pgp
-Implementing basic function of PGP for Computer Security class
+Programmer: Gideon Walker 12450191
+Class: CS 3800 Computer Security
+Assignment: Final Project - Implementing Authentication and
+  Confidentiality Services in OpenPGP
+Date: 12/15/17
+
+Purpose of files:
++ gwalker_pgp.cpp - runs main program
++ example_plaintext.txt - the example message input. NOTE: Program does not
+generate message but rather reads it in from a file. Make sure to spell it
+correctly or the program will fail.
++ Makefile - compiles the program. NOTE: make clean is available.
+
+How to compile and run.
+1. `make` or `g++ -std=c++11 gwalker_pgp.cpp -o program -lgmp -lcrypto`
+2. `./program`
+3. Generates the several files required for the assignment after inputing the
+file names.
--- gwalker_pgp.cpp
+++ gwalker_pgp.cpp
@@ -1,3 +1,11 @@
+/*
+Programmer: Gideon Walker 12450191
+Class: CS 3800 Computer Security
+Assignment: Final Project - Implementing Authentication and
+  Confidentiality Services in OpenPGP
+Date: 12/15/17
+*/
+
 #include <stdio.h>
 #include <gmp.h>
 #include <algorithm>
@@ -16,64 +24,62 @@
 #include <fstream>
 using namespace std;
 
-const bool DEBUG = true;
+const bool DEBUG = false;
 
-RSA* generate_key(string key_fn)
-{
-    int             ret = 0;
-    RSA             *r = NULL;
-    BIGNUM          *bne = NULL;
-    BIO             *bp_file = NULL;
+RSA* generate_key(string key_fn){
+  int ret = 0;
+  RSA *r = NULL;
+  BIGNUM *bne = NULL;
+  BIO *bp_file = NULL;
 
-    int             bits = 2048;
-    unsigned long   e = RSA_F4;
-    FILE * key_file = NULL;
-    char * bignum_dec = NULL;
+  int bits = 2048;
+  unsigned long e = RSA_F4;
+  FILE * key_file = NULL;
+  char * bignum_dec = NULL;
 
-    // 1. generate rsa key
-    bne = BN_new();
-    ret = BN_set_word(bne,e);
-    if(ret != 1){
-        goto free_all;
-    }
-
-    r = RSA_new();
-    ret = RSA_generate_key_ex(r, bits, bne, NULL);
-    if(ret != 1){
-        goto free_all;
-    }
-
-    // 2. save public key
-    bp_file = BIO_new_file(key_fn.c_str(), "w+");
-    ret = PEM_write_bio_RSAPublicKey(bp_file, r);
-    if(ret != 1){
-        goto free_all;
-    }
-
-    // write out the prime number n
-    key_file = fopen(key_fn.c_str(), "a");
-    if(key_file == NULL){
-      perror("Erorr opening file.");
+  // Make a new RSA key
+  bne = BN_new();
+  ret = BN_set_word(bne,e);
+  if(ret != 1){
       goto free_all;
-    }
-    else{
-      fprintf(key_file, "n = %d\n", e);
-    }
+  }
 
-    // 3. save private key
-    ret = PEM_write_bio_RSAPrivateKey(bp_file, r, NULL, NULL, 0, NULL, NULL);
+  r = RSA_new();
+  ret = RSA_generate_key_ex(r, bits, bne, NULL);
+  if(ret != 1){
+      goto free_all;
+  }
 
-    // 4. free
+  // Write out the public key
+  bp_file = BIO_new_file(key_fn.c_str(), "w+");
+  ret = PEM_write_bio_RSAPublicKey(bp_file, r);
+  if(ret != 1){
+      goto free_all;
+  }
+
+  // write out the prime number n
+  key_file = fopen(key_fn.c_str(), "a");
+  if(key_file == NULL){
+    perror("Erorr opening file.");
+    goto free_all;
+  }
+  else{
+    fprintf(key_file, "n = %d\n", e);
+  }
+
+  // Write out the private key.
+  ret = PEM_write_bio_RSAPrivateKey(bp_file, r, NULL, NULL, 0, NULL, NULL);
+
+  // Free pointers
 free_all:
+  BIO_free_all(bp_file);
+  BN_free(bne);
+  fclose(key_file);
 
-    BIO_free_all(bp_file);
-    BN_free(bne);
-    fclose(key_file);
-
-    return r;
+  return r;
 }
 
-// Returns a readable version of encrytped/hashed char*
+// Returns a readable hex version of encrytped/hashed char*
 string get_string(unsigned char * stuff){
   char mdString[SHA512_DIGEST_LENGTH*2+1];
   for (int i = 0; i < SHA512_DIGEST_LENGTH; i++)
@@ -100,19 +106,26 @@ int main (){
 
   // Get file names
   if(! DEBUG){
-    cout << "Enter the name of the file that contains Alice’s public-private key pair:" << endl;
+    cout << "Enter the name of the file that contains "
+         << "Alice’s public-private key pair:" << endl;
     cin >> alice_keys_fn;
-    cout << "Enter the name of the file that contains Bob’s public-private key pair:" << endl;
+    cout << "Enter the name of the file that contains "
+         << "Bob’s public-private key pair:" << endl;
     cin >> bob_keys_fn;
-    cout << "Enter the name of the file that contains Alice’s plaintext message:" << endl;
+    cout << "Enter the name of the file that contains "
+         << "Alice’s plaintext message:" << endl;
     cin >> alice_text_fn;
-    cout << "Enter the output file name to store Alice’s authenticated message:" << endl;
+    cout << "Enter the output file name to store Alice’s "
+         << "authenticated message:" << endl;
     cin >> alice_auth_fn;
-    cout << "Enter the output file name to store the verification steps performed by Bob:" << endl;
+    cout << "Enter the output file name to store the verification "
+         << "steps performed by Bob:" << endl;
     cin >> bob_verify_fn;
-    cout << "Enter the output file name to store Alice’s encrypted message:" << endl;
+    cout << "Enter the output file name to store Alice’s "
+         << "encrypted message:" << endl;
     cin >> alice_encrypt_fn;
-    cout << "Enter the output file name to store Bob’s decryption of Alice’s plaintext message:" << endl;
+    cout << "Enter the output file name to store Bob’s decryption "
+         << "of Alice’s plaintext message:" << endl;
     cin >> bob_decrypt_fn;
     cout << endl;
   }
@@ -120,6 +133,7 @@ int main (){
   //----------------
   // MESSAGE DIGEST
   //----------------
+  // Grab the contents of the plaintext message from the file
   ifstream plaintext_f(alice_text_fn);
   string plaintext;
   string file_input;
@@ -132,8 +146,9 @@ int main (){
   }
   else cout << "Error opening " << alice_text_fn << endl;
   plaintext_f.close();
-  if(DEBUG) cout << "plaintext:" << endl << plaintext << endl;
+  if(DEBUG) cout << "Plaintext:" << endl << plaintext << endl;
 
+  // Create a message digest by hashing with SHA512
   unsigned char digest[SHA512_DIGEST_LENGTH];
   SHA512_CTX ctx;
   SHA512_Init(&ctx);
@@ -152,17 +167,18 @@ int main (){
     if(DEBUG) cout << "Failure with malloc." << endl;
   }
 
-  signiture_len = RSA_private_encrypt(sizeof digest, digest, signiture, alice_keys, RSA_PKCS1_PADDING);
+  // Make a signiture from the message digest with Alice's private key
+  signiture_len = RSA_private_encrypt(sizeof digest, digest, signiture,
+                                      alice_keys, RSA_PKCS1_PADDING);
   signiture_str = get_string(signiture);
-  if(DEBUG){cout << "Alice's signiture of message:" << endl << signiture_str << endl << endl;}
+  if(DEBUG) cout << "Alice's signiture of message:\n" << signiture_str << "\n\n";
 
+  // Output to file
   ofstream alice_auth(alice_auth_fn);
-  if(alice_auth){
+  if(alice_auth)
     alice_auth << plaintext << signiture_str << "\n";
-  }
-  else{
-    cout << "Error opening " << alice_auth_fn << endl;
-  }
+  else
+    cout << "Error opening " << alice_auth_fn << "\n";
   alice_auth.close();
 
   //-----------------
@@ -172,21 +188,24 @@ int main (){
   unsigned int decrypted_len = 0;
   decrypted = (unsigned char *)malloc(RSA_size(alice_keys));
 
-  decrypted_len = RSA_public_decrypt(signiture_len, signiture, decrypted, alice_keys, RSA_PKCS1_PADDING);
+  // Decrypte the signiture w/ Alice's public key
+  decrypted_len = RSA_public_decrypt(signiture_len, signiture, decrypted,
+                                     alice_keys, RSA_PKCS1_PADDING);
   decrypted_str = get_string(decrypted);
-  if(DEBUG) cout << "Heres the decrypted string: " << endl << decrypted_str << endl;
+  if(DEBUG) cout << "Heres the decrypted string:\n"<< decrypted_str << "\n";
   if(DEBUG){
-    if(decrypted_str == digest_str) cout << "The strings match!" << endl << endl;
-    else cout << ":( the strings do not match." << endl << endl;
+    if(decrypted_str == digest_str) cout << "The strings match!" << "\n\n";
+    else cout << ":( the strings do not match." << "\n\n";
   }
 
+  // Output to file
   ofstream bob_verify(bob_verify_fn);
   if(bob_verify){
-    bob_verify << "Digest line 2, Decrypted line 3:\n" << digest_str << "\n" << decrypted_str << "\n";
+    bob_verify << "Digest line 2, Decrypted line 3:\n"
+               << digest_str << "\n" << decrypted_str << "\n";
   }
-  else{
-    cout << "Problem opening " << bob_verify_fn << endl;
-  }
+  else
+    cout << "Problem opening " << bob_verify_fn << "\n";
   bob_verify.close();
 
   //----------------------
@@ -198,12 +217,13 @@ int main (){
   RAND_bytes(initial_vector, sizeof(initial_vector));
   unsigned char *key_char = (unsigned char *)key;
   string key_char_str = get_string(key_char);
-  if(DEBUG) cout << "Session Key:" << endl << key_char_str << endl << endl;
+  if(DEBUG) cout << "Session Key:\n" << key_char_str << "\n\n";
 
   // Encrypt the session key with Bob's public key
-  signiture_len = RSA_public_encrypt(64, key_char, signiture, bob_keys, RSA_PKCS1_PADDING);
+  signiture_len = RSA_public_encrypt(64, key_char, signiture, bob_keys,
+                                     RSA_PKCS1_PADDING);
   signiture_str = get_string(signiture);
-  if(DEBUG) cout << "Encrypted session key:" << endl << signiture_str << endl << endl;
+  if(DEBUG) cout << "Encrypted session key:\n" << signiture_str << "\n\n";
 
   // A copy of the initialization vector is needed since it is destroyed
   uint8_t initial_vector2[AES_BLOCK_SIZE];
@@ -226,9 +246,10 @@ int main (){
   const int padded_text_size = (const int)padded_text.size();
 
   unsigned char encrypted_data[512] = {0};
-  AES_cbc_encrypt(padded_text_char, encrypted_data, padded_text_size, (const AES_KEY*)aes_key, initial_vector, AES_ENCRYPT);
+  AES_cbc_encrypt(padded_text_char, encrypted_data, padded_text_size,
+                  (const AES_KEY*)aes_key, initial_vector, AES_ENCRYPT);
   string encrypted_str = get_string(encrypted_data);
-  cout << "encrypted data: " << endl << encrypted_str << endl << endl;
+  cout << "Encrypted data:\n" << encrypted_str << "\n\n";
 
   //---------------
   // BOB - RECIEVE SESSION KEY AND DECRYPT MESSAGE
@@ -237,28 +258,40 @@ int main (){
   AES_set_decrypt_key(key, 256, aes_decrypt_key);
 
   // Decrypt the session key that Alice sent
-  decrypted_len = RSA_private_decrypt(signiture_len, signiture, decrypted, bob_keys, RSA_PKCS1_PADDING);
+  decrypted_len = RSA_private_decrypt(signiture_len, signiture, decrypted,
+                                      bob_keys, RSA_PKCS1_PADDING);
   decrypted_str = get_string(decrypted);
   if(DEBUG) cout << "Decrypted session key:" << endl << decrypted_str << endl;
   if(DEBUG){
     string msg;
     decrypted_str == key_char_str ? msg="Same Key!" : msg="Different key...";
-    cout << msg << endl << endl;
+    cout << msg << "\n\n";
   }
 
+  // Decrypt the encrypted data
   unsigned char decrypted_data[512] = {0};
-  AES_cbc_encrypt(encrypted_data, decrypted_data, padded_text_size, (const AES_KEY*)aes_decrypt_key, initial_vector2, AES_DECRYPT);
-  for(int i = 0; i<512; i++){
-    printf("%c", decrypted_data[i]);
+  AES_cbc_encrypt(encrypted_data, decrypted_data, padded_text_size,
+                  (const AES_KEY*)aes_decrypt_key, initial_vector2, AES_DECRYPT);
+  if(DEBUG){
+    cout << "Decrypted message:" << endl;
+    for(int i = 0; i<512; i++){
+      printf("%c", decrypted_data[i]);
+    }
+    printf("\n");
   }
-  printf("\n");
+
+  // Output to file
+  FILE *bob_decrypt;
+  bob_decrypt = fopen(bob_decrypt_fn.c_str(), "w+");
+  for(int i = 0; i<plaintext.size(); i++){
+    fprintf(bob_decrypt, "%c", decrypted_data[i]);
+  }
+  fprintf(bob_decrypt, "\n");
+  fclose(bob_decrypt);
 
   // free up the pointers
   RSA_free(alice_keys);
   RSA_free(bob_keys);
-  //delete aes_key;
-  //delete aes_decrypt_key;
-  //delete padded_text_char;
   delete signiture;
   delete decrypted;
   return 0;
